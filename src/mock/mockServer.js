cada6e644c562bf0f901ce06ae42ab7a266888e9
kcz <1150791676@qq.com>
2020-03-01 12:42:21 +0800

    添加mockServer

---
+++ src/mock/mockServer.js
@@ -0,0 +1,29 @@
+/*
+ * @Description: 用于启动mock服务文件
+ * @Author: kcz
+ * @Date: 2020-03-01 11:55:54
+ * @LastEditors: kcz
+ * @LastEditTime: 2020-03-01 12:35:03
+ */
+// const mock2 = require('./index')
+const Koa = require('koa')
+const routes = require('./router.config')
+const KoaRouter = require('koa-router')
+const router = new KoaRouter()
+const app = new Koa()
+const port = 9090 // 服务端口
+
+app.listen(port)
+console.log('服务启动成功', `http://localhost:${port}/`)
+
+router.use('/', async (ctx, next) => {
+  try {
+    await next()
+  } catch (e) {
+    ctx.throw(500, 'Internal Server Error')
+  }
+})
+
+router.use(routes)
+
+app.use(router.routes())
