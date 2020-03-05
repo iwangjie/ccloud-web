ae77a1f5885911612abca90889b84bd29a1c4617
wangjie <wangchenmo2015@gmail.com>
2020-03-05 14:18:44 +0800

    基础菜单

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -46,6 +46,178 @@ export const asyncRouterMap = [
             component: () => import('@/views/home/index')
           }
         ]
+      },
+      {
+        path: '/registrationCode',
+        name: 'RegistrationCode',
+        meta: {
+          title: '注册码管理',
+          icon: 'file-text'
+        },
+        component: RouterView,
+        children: [
+          {
+            path: '/makeRegistrationCode',
+            name: 'test2',
+            meta: {
+              title: '生成注册码',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }, {
+            path: '/registrationCodeList',
+            name: 'test2',
+            meta: {
+              title: '注册码列表',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }
+        ]
+      },
+      {
+        path: '/updateVersion',
+        name: 'UpdateVersion',
+        meta: {
+          title: '更新管理',
+          icon: 'file-text'
+        },
+        component: RouterView,
+        children: [
+          {
+            path: '/addVersion',
+            name: 'test2',
+            meta: {
+              title: '发布版本',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }, {
+            path: '/versionList',
+            name: 'test2',
+            meta: {
+              title: '版本列表',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }
+        ]
+      },
+      {
+        path: '/customDevelopment',
+        name: 'CustomDevelopment',
+        meta: {
+          title: '动态扩展',
+          icon: 'file-text'
+        },
+        component: RouterView,
+        children: [
+          {
+            path: '/addCustomDevelopment',
+            name: 'test2',
+            meta: {
+              title: '发布插件',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }, {
+            path: '/customDevelopmentList',
+            name: 'test2',
+            meta: {
+              title: '插件管理',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }
+        ]
+      },
+      {
+        path: '/logAdmin',
+        name: 'LogAdmin',
+        meta: {
+          title: '日志管理',
+          icon: 'file-text'
+        },
+        component: RouterView,
+        children: [
+          {
+            path: '/accessLog',
+            name: 'accessLog',
+            meta: {
+              title: '接口访问日志',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }, {
+            path: '/activationLog',
+            name: 'activationLog',
+            meta: {
+              title: '注册码激活日志',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }
+        ]
+      },
+      {
+        path: '/feedback',
+        name: 'Feedback',
+        meta: {
+          title: '留言反馈管理',
+          icon: 'file-text'
+        },
+        component: RouterView,
+        children: [
+          {
+            path: '/feedbackList',
+            name: 'feedbackList',
+            meta: {
+              title: '留言反馈列表',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }
+        ]
+      },
+      {
+        path: '/analysis',
+        name: 'Analysis',
+        meta: {
+          title: '数据分析管理',
+          icon: 'file-text'
+        },
+        component: RouterView,
+        children: [
+          {
+            path: '/userAnalysis',
+            name: 'userAnalysis',
+            meta: {
+              title: '用户分析',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }
+        ]
+      },
+      {
+        path: '/system',
+        name: 'Analysis',
+        meta: {
+          title: '系统设置',
+          icon: 'file-text'
+        },
+        component: RouterView,
+        children: [
+          {
+            path: '/UserConfig',
+            name: 'userAnalysis',
+            meta: {
+              title: '个性化设置',
+              icon: 'file-text'
+            },
+            component: () => import('@/views/home/index')
+          }
+        ]
       }
     ]
   },
